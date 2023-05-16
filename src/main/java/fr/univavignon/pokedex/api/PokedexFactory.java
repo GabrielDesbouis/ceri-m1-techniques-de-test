7d08883d02e7de059eec34d99f8205e63a2c1fff
GabrielDesbouis <90773358+GabrielDesbouis@users.noreply.github.com>
2023-05-16 02:08:49 +0200

    implements PokedexFactory

---
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactory.java
@@ -0,0 +1,16 @@
+package fr.univavignon.pokedex.api;
+
+public class PokedexFactory
+        implements IPokedexFactory {
+    /**
+     * @param metadataProvider Metadata provider the created pokedex will use.
+     * @param pokemonFactory   Pokemon factory the created pokedex will use.
+     * @return Created pokedex instance.
+     */
+    @Override
+    public IPokedex createPokedex(
+            final IPokemonMetadataProvider metadataProvider,
+            final IPokemonFactory pokemonFactory) {
+        return null;
+    }
+}
